ee0a400aaa5cf69c2f5df3f1028fb0aac5f9502b
Ivor Wanders <ivor@iwanders.net>
2023-11-22 18:26:29 -0500

    Actually use hostname.

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "papyrus"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
