701c3545461a62f4b590e1e2832a72e0f273dc29
Ivor Wanders <ivor@iwanders.net>
2023-11-22 20:12:52 -0500

    Split out gnome configuration, add Nautilus.

--- configuration.nix
+++ configuration.nix
@@ -11,8 +11,8 @@
 
   imports =
     [
-      # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./module-gnome-minimal.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -46,37 +46,7 @@
     "/nix".options = [ "compress=zstd" "noatime" ];
   };
 
-  # Configure keymap in X11
-  services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  services.gnome.core-utilities.enable = false;
-  # well, that still pulls in orca, with speech synthesis, for many megabytes.
-  # https://github.com/NixOS/nixpkgs/blob/4ecab3273592f27479a583fb6d975d4aba3486fe/nixos/modules/services/x11/desktop-managers/gnome.nix#L459
-  # https://discourse.nixos.org/t/howto-disable-most-gnome-default-applications-and-what-they-are/13505
-  environment.gnome.excludePackages = (with pkgs.gnome; [
-    baobab      # disk usage analyzer
-    eog         # image viewer
-    epiphany    # web browser
-    gedit       # text editor
-    simple-scan # document scanner
-    totem       # video player
-    yelp        # help viewer
-    evince      # document viewer
-    file-roller # archive manager
-    geary       # email client
-    seahorse    # password manager
-    gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-contacts
-    gnome-font-viewer gnome-logs gnome-maps gnome-music gnome-screenshot
-    gnome-system-monitor gnome-weather gnome-disk-utility pkgs.gnome-connections
-  ]) ++ (with pkgs;[
-    orca
-  ]);
-  # We also lose nautilus now though...
 
   # Enable sound.
   sound.enable = true;
---
+++ module-gnome-minimal.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+  ];
+
+  options = {
+  };
+
+  config = {
+    # https://github.com/NixOS/nixpkgs/blob/4ecab3273592f27479a583fb6d975d4aba3486fe/nixos/modules/services/x11/desktop-managers/gnome.nix#L459
+
+    # Configure keymap in X11
+    services.xserver.layout = "us";
+    # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+    services.xserver.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+
+    services.gnome.core-utilities.enable = false;
+    # well, that still pulls in orca, with speech synthesis, for many megabytes.
+    # https://github.com/NixOS/nixpkgs/blob/4ecab3273592f27479a583fb6d975d4aba3486fe/nixos/modules/services/x11/desktop-managers/gnome.nix#L459
+    # https://discourse.nixos.org/t/howto-disable-most-gnome-default-applications-and-what-they-are/13505
+    environment.gnome.excludePackages = (with pkgs.gnome; [
+      baobab      # disk usage analyzer
+      eog         # image viewer
+      epiphany    # web browser
+      gedit       # text editor
+      simple-scan # document scanner
+      totem       # video player
+      yelp        # help viewer
+      evince      # document viewer
+      file-roller # archive manager
+      geary       # email client
+      seahorse    # password manager
+      gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-contacts
+      gnome-font-viewer gnome-logs gnome-maps gnome-music gnome-screenshot
+      gnome-system-monitor gnome-weather gnome-disk-utility pkgs.gnome-connections
+    ]) ++ (with pkgs;[
+      orca
+    ]);
+    # We also lose nautilus now though...
+    environment.systemPackages =
+      with pkgs.gnome; [
+        nautilus
+    ];
+  };
+}
