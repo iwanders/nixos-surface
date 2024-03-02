0c2432b40875b64a23629a92b9bf3c78d119f1d0
Ivor Wanders <ivor@iwanders.net>
2024-03-02 18:12:24 -0500

    Move kernel_dev to the linux-surface-dev repo.

--- kernel_dev/dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This totally not nixy way... should be `nix develop .`
-nix develop "$(realpath /run/booted-system/flake)#nixosConfigurations.$(hostname).config.boot.kernelPackages.kernel"
-export KERNELDIR=$(nix build --print-out-paths "$(realpath /run/booted-system/flake)#nixosConfigurations.papyrus.config.boot.kernelPackages.kernel.dev")/lib/modules/*/build
