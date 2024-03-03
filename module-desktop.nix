43b25e6ae320d7745bd7e6709b33038f2d57f5b1
Ivor Wanders <ivor@iwanders.net>
2024-03-03 13:15:04 -0500

    Settings for gdm.

--- module-desktop.nix
+++ module-desktop.nix
@@ -11,12 +11,38 @@
     # services.xserver.xkbOptions = "eurosign:e,caps:escape";
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.displayManager.gdm.debug = true;
+
+    # This block is here to ensure we get GDM with the custom on screen keyboard extension
+    # in the settings I want.
+    programs.dconf.profiles.gdm.databases = [
+      {
+        lockAll = true;
+        settings."org/gnome/shell/extensions/enhancedosk" = {
+          show-statusbar-icon=true;
+          locked=true;
+        };
+      }
+
+      {
+        lockAll = true;
+        settings."org/gnome/shell" = {
+          enabled-extensions=["iwanders-gnome-enhanced-osk-extension"];
+        };
+      }
+
+      {
+        lockAll = true;
+        settings."org/gnome/desktop/a11y/applications" = {
+          screen-keyboard-enabled=true;
+        };
+      }
+    ];
+
     services.xserver.desktopManager.gnome.enable = true;
 
     services.gnome.core-utilities.enable = false;
 
-    # well, that still pulls in orca, with speech synthesis, for many megabytes.
+    # well, that (utilities false) still pulls in orca, with speech synthesis, for many megabytes.
     # https://github.com/NixOS/nixpkgs/blob/4ecab3273592f27479a583fb6d975d4aba3486fe/nixos/modules/services/x11/desktop-managers/gnome.nix#L459
     # https://discourse.nixos.org/t/howto-disable-most-gnome-default-applications-and-what-they-are/13505
     environment.gnome.excludePackages = (with pkgs.gnome; [
@@ -44,7 +70,7 @@
       pkgs.gnome-connections
     ]) ++ (with pkgs; [ orca ]);
 
-    # We also lose nautilus now though...
+    # We also lose nautilus now though, so we add back stuff we actually care about...
     environment.systemPackages = (with pkgs.gnome; [
       eog # image viewer
       gnome-terminal
