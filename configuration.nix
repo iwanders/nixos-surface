86503343a9902509dc1164bad3db399419989f55
Ivor Wanders <ivor@iwanders.net>
2024-03-02 20:01:48 -0500

    Add kernel modules for typecover at initrd stage.

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,20 @@
     # https://github.com/linux-surface/linux-surface/issues/862
     "i915.enable_psr=0"
   ];
+  # Add the kernel modules such that we have a working keyboard for the 
+  # LUKS full disk encryption.
+  # https://github.com/linux-surface/linux-surface/wiki/Disk-Encryption
+  boot.initrd.kernelModules = [
+    "surface_aggregator"
+    "surface_aggregator_registry"
+    "surface_aggregator_hub"
+    "surface_hid_core"
+    "surface_hid"
+    "pinctrl_tigerlake"
+    "intel_lpss"
+    "intel_lpss_pci"
+    "8250_dw"
+  ];
 
   # enable core dumps.
   systemd.coredump.enable = true;
